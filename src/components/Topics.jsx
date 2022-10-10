eeabfda286e9f317608e10632b956ec643dc4db3
kskanak <kskanak26@gmail.com>
2022-10-11 01:12:25 +0600

    topics component and content added

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TopicsContext } from "../layout/Root";
+import Topic from "./Topic";
 
 const Topics = () => {
+  const topics = useContext(TopicsContext);
   return (
-    <div>
-      <h2>topics</h2>
+    <div className="bg-gray-400">
+      <div className="topics-header text-center py-5 text-3xl font-semibold">
+        <h2 className="">Topics List</h2>
+      </div>
+      <div className="  topics grid md:grid-cols-2 lg:grid-cols-4 pl-10 lg:pl-5 lg:pr-4  py-10">
+        {topics.map((topic) => (
+          <Topic key={topic.id} topic={topic}></Topic>
+        ))}
+      </div>
     </div>
   );
 };
